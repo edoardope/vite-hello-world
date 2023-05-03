7520be51f548c108f5f805f1c858763139d5e525
edoardope <pencoedoardo@gmail.com>
2023-05-03 02:40:44 +0200

    1

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+  import AppTitle from "./components/AppTitle.vue"
+
+  export default {
+    name: "app",
+    components: {
+      "AppTitle",
+    },
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+
+    }
+  }
+</script>
+
+<template>
+  <AppTitle/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,23 @@
+<script>
+
+  export default{
+      name: "AppTitle",
+      data(){
+          return(
+
+          )
+      },
+      methods: {
+
+      }
+  }
+
+</script>
+
+<template>
+   <h1>La mia prima app con Vite!</h1>
+</template>
+
+<style scoped>
+
+</style>
